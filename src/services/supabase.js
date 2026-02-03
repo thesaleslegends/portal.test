c61c03468a4135a7f3c22622c4cc2c4dfef004a4
Kees Van den nieuwenhoff <keesvandennieuwenhoff@MacBookPro.home>
2026-02-03 09:40:42 +0100

    Fix supabasde

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const SUPABASE_URL = "https://vvxjeipsrtndohvfxnvb.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ2eGplaXBzcnRuZG9odmZ4bnZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg2NTE0ODIsImV4cCI6MjA4NDIyNzQ4Mn0.5UwRJC-XR7-KggeL9Kx9FikVIYSK_9u7mRAGEjAXFTQ";
+const SUPABASE_URL = "https://wshykrbawnwimzifzraz.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndzaHlrcmJhd253aW16aWZ6cmF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk3Njc4MDEsImV4cCI6MjA4NTM0MzgwMX0.ICeJWgJcGuPD2ZvpSnJdLu52JIYFshSy3PJXDm_hjds";
 
 export const supabase = createClient(
   SUPABASE_URL,
